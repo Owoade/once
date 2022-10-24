2fb6ac141df03b49379cbfe110a6febf5bc64c99
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2022-10-24 16:55:06 -0700

    pushed to heroku

---
+++ src/models/transaction.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const transactionSchema = new mongoose_1.default.Schema({
+    amount: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    ref: {
+        type: String,
+        required: true
+    },
+    access_code: {
+        type: String
+    }
+}, { timestamps: true });
+const Transaction = mongoose_1.default.model("Transaction", transactionSchema);
+exports.default = Transaction;
