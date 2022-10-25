7006252e0a4ecea728af478f5da9ccfee83b30dd
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2022-10-24 17:27:53 -0700

    pushed to heroku

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const initializer_1 = require("./sdks/initializer");
 const express_1 = __importDefault(require("express"));
@@ -32,7 +33,7 @@ transactionNamspace.on("connection", (socket) => {
 });
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-const PORT = 3000 !== null && 3000 !== void 0 ? 3000 : process.env.PORT;
+const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
 app.post("/init", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { amount } = req.body;
     const checkout = yield initializer_1.once.initialize(parseInt(amount));
@@ -44,8 +45,8 @@ app.get("/checkout", (req, res) => __awaiter(void 0, void 0, void 0, function* (
     res.json(providerCheckout);
 }));
 app.patch("/update-transaction", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    const update = (_a = req.body) === null || _a === void 0 ? void 0 : _a.update;
+    var _b;
+    const update = (_b = req.body) === null || _b === void 0 ? void 0 : _b.update;
     const id = req.body.id;
     const updatedTransaction = yield transaction_1.default.findByIdAndUpdate(id, update, { new: true });
     res.json({ message: "successfull" });
@@ -60,6 +61,9 @@ app.post("/payment-webhook", (req, res) => __awaiter(void 0, void 0, void 0, fun
     console.log(req.body);
     res.end();
 }));
-const promiseArr = [mongoose_1.default.connect(process.env.MONGO_DB_URL), app.listen(PORT)];
-Promise.all(promiseArr)
-    .then(() => console.log("Server is up and running"));
+// const promiseArr = [ mongoose.connect(process.env.MONGO_DB_URL as string), app.listen(PORT) ];
+// Promise.all( promiseArr )
+// .then( ()=> console.log("Server is up and running") )
+mongoose_1.default.connect(process.env.MONGO_DB_URL)
+    .then(() => console.log("Mongo is live"));
+app.listen(PORT, () => console.log("APP is live"));
